6c3af59ebcc2e67f7c0c26170f35955c1ca47abb
johnnjoro <johnnotes4@gmail.com>
2023-02-22 09:54:51 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-sign.c -o 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - function thatprints signs
+ * @n: function parameter
+ *
+ * Return: 1,0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+, ");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0, ");
+		return (0);
+	}
+	else
+	{
+		printf("-, ");
+		return (-1);
+	}
+
+
+}

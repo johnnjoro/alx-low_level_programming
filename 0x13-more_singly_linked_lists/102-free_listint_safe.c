a58e6eb9643606beb1b4df46fd07f8e3327a8220
johnnjoro <johnnotes4@gmail.com>
2023-04-05 07:34:56 -0700

    changes

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -56,4 +56,10 @@ size_t free_listint_safe(listint_t **head)
 		}
 		num++;
 		list = _ra(list, num, *head);
-
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
+}
